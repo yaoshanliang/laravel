87d534054cb89f559de84ea629eab7768770864e
iat <i@iat.net.cn>
2017-03-06 16:20:46 +0800

    add vendor assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const elixir = require('laravel-elixir');
 
-require('laravel-elixir-vue-2');
+// require('laravel-elixir-vue-2');
 
 /*
  |--------------------------------------------------------------------------
@@ -14,6 +14,57 @@ require('laravel-elixir-vue-2');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+
+    // web
+    mix.scripts([
+    './resources/assets/vendor/jquery/jquery.min.js',
+    './resources/assets/vendor/bootstrap/bootstrap.min.js',
+], 'public/web-assets/js/lib.js');
+
+mix.styles([
+    './resources/assets/vendor/bootstrap/bootstrap.min.css',
+], 'public/web-assets/css/lib.css');
+
+mix.copy('./resources/assets/vendor/fonts/', 'public/web-assets/fonts');
+mix.copy('./resources/assets/vendor/fonts/', 'public/web-assets/fonts');
+
+mix.scripts([
+    './resources/assets/web/web.js'
+], 'public/web-assets/js/web.js');
+mix.styles([
+    './resources/assets/web/web.css'
+], 'public/web-assets/css/web.css');
+
+mix.version(['web-assets/js/web.js', 'web-assets/css/web.css']);
+
+
+//admin
+mix.scripts([
+    './resources/assets/vendor/jquery/jquery.min.js',
+    './resources/assets/vendor/bootstrap/bootstrap.min.js',
+    './resources/assets/vendor/datatables/datatables.min.js',
+    './resources/assets/vendor/gentelella/custom.min.js',
+], 'public/admin-assets/js/lib.js');
+
+mix.styles([
+    './resources/assets/vendor/bootstrap/bootstrap.min.css',
+    './resources/assets/vendor/fonts/font-awesome.min.css',
+    './resources/assets/vendor/datatables/datatables.min.css',
+    './resources/assets/vendor/gentelella/custom.min.css',
+], 'public/admin-assets/css/lib.css');
+
+mix.copy('./resources/assets/vendor/fonts/', 'public/admin-assets/fonts');
+mix.copy('./resources/assets/vendor/fonts/', 'public/admin-assets/fonts');
+
+// simditor
+// mix.copy('./resources/assets/vendor/simditor', 'public/admin-assets/vendor/simditor');
+
+mix.scripts([
+    './resources/assets/admin/admin.js'
+], 'public/admin-assets/js/admin.js');
+mix.styles([
+    './resources/assets/admin/admin.css'
+], 'public/admin-assets/css/admin.css');
+
+mix.version(['admin-assets/js/admin.js', 'admin-assets/css/admin.css']);
 });
---
+++ resources/views/admin/auth/login.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <body class="login">
+    <div>
+        <div class="login_wrapper">
+            <div class="animate form login_form">
+                <section class="login_content">
+                    <form method="post" action="{{ url('/login') }}">
+                        {!! csrf_field() !!}
+
+                        <h1>Login Form</h1>
+                        <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
+                            <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
+                            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                      <strong>{{ $errors->first('email') }}</strong>
+                </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <input type="password" class="form-control" placeholder="Password" name="password">
+                            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                  <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                            @endif
+
+                        </div>
+                        <div>
+                            <input type="submit" class="btn btn-default submit" value="Log in">
+                            <a class="reset_pass" href="{{  url('/password/reset') }}">Lost your password?</a>
+                        </div>
+
+                        <div class="clearfix"></div>
+
+                        <div class="separator">
+                            <p class="change_link">New to site?
+                                <a href="{{ url('/register') }}" class="to_register"> Create Account </a>
+                            </p>
+
+                            <div class="clearfix"></div>
+                            <br />
+
+                            <div>
+                                <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                                <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </div>
+    </div>
+    </body>
+@endsection
