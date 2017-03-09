8d75ed2cb60e18adf239950647e2571e3592a8f8
iat <i@iat.net.cn>
2017-03-09 09:06:10 +0800

    update

--- resources/views/web/common/common.blade.php
+++ resources/views/web/common/common.blade.php
@@ -16,10 +16,6 @@
 
     <script src="{{ asset('web-assets/js/lib.js') }}"></script>
 
-    <!--[if lt IE 9]>
-    <script src="../assets/js/ie8-responsive-file-warning.js"></script>
-    <![endif]-->
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
