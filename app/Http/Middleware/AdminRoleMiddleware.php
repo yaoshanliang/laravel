d4339ca0d447fecd733f8e4422a7dc1278b3bbf9
iat <i@iat.net.cn>
2017-04-12 09:56:22 +0800

    add admin role middleware

---
+++ app/Http/Middleware/AdminRoleMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AdminRoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $guard = 'admin';
+
+        if (Auth::guard($guard)->guest()) {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response('Unauthorized.', 401);
+            } else {
+                return redirect()->guest(url('admin/auth/login'));
+            }
+        }
+
+        return $next($request);
+    }
+}
