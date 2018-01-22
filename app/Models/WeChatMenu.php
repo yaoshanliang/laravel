97ab9e8726d268d109a303d80402b1c4b57383c7
binbin <zhangbinbin>
2018-01-22 14:22:35 +0800

    菜单bug修复

--- app/Models/WeChatMenu.php
+++ app/Models/WeChatMenu.php
@@ -6,6 +6,7 @@ use App\Models\Model;
 
 class WeChatMenu extends Model
 {
+    protected $table = 'wechat_menus';
     protected $fillable = [
         'pid', 'type', 'title', 'url', 'key', 'has_sub'
     ];
