dde17d834c54a6bdbeeb76e82627e1d71bc23460
binbin <zhangbinbin>
2018-01-26 10:14:30 +0800

    测试menu

--- app/Http/Controllers/WeChat/MenuController.php
+++ app/Http/Controllers/WeChat/MenuController.php
@@ -16,28 +16,30 @@ class MenuController extends Controller
 
     public function setMenu()
     {
-        $buttons = [];
-        $level1 = Menu::where(['pid' => 0])->orderBy('sort')->get();
-        foreach($level1 as $k => $v) {
-            if ($v->has_sub == 1) {
-                $buttons[$k]['name'] = $v->title;
-                $buttons[$k]['sub_button'] = [];
-                $level2 = Menu::where(['pid' => $v->id])->orderBy('sort')->get();
-                foreach($level2 as $key => $val) {
-                    $buttons[$k]['sub_button'][$key]['type'] = 'view';
-                    $buttons[$k]['sub_button'][$key]['name'] = $val->title;
-                    $buttons[$k]['sub_button'][$key]['url'] = $val->url;
-                }
-            } else {
-                $buttons[$k]['type'] = 'view';
-                $buttons[$k]['name'] = $v->title;
-                $buttons[$k]['url'] = $v->url;
-            }
-        }
-        if ($this->menu->create($buttons)) {
-            echo 'success';
-        } else {
-            echo 'fail';
-        }
+        $list = $this->menu->list();
+        dd($list);
+//        $buttons = [];
+//        $level1 = Menu::where(['pid' => 0])->orderBy('sort')->get();
+//        foreach($level1 as $k => $v) {
+//            if ($v->has_sub == 1) {
+//                $buttons[$k]['name'] = $v->title;
+//                $buttons[$k]['sub_button'] = [];
+//                $level2 = Menu::where(['pid' => $v->id])->orderBy('sort')->get();
+//                foreach($level2 as $key => $val) {
+//                    $buttons[$k]['sub_button'][$key]['type'] = 'view';
+//                    $buttons[$k]['sub_button'][$key]['name'] = $val->title;
+//                    $buttons[$k]['sub_button'][$key]['url'] = $val->url;
+//                }
+//            } else {
+//                $buttons[$k]['type'] = 'view';
+//                $buttons[$k]['name'] = $v->title;
+//                $buttons[$k]['url'] = $v->url;
+//            }
+//        }
+//        if ($this->menu->create($buttons)) {
+//            echo 'success';
+//        } else {
+//            echo 'fail';
+//        }
     }
 }
