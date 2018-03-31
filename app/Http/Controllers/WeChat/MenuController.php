e01aaaeb752f72134fbc528ee78e67a15eb56b15
iat <i@iat.net.cn>
2018-03-31 13:36:37 +0800

    同步菜单时增加小程序类型

--- app/Http/Controllers/WeChat/MenuController.php
+++ app/Http/Controllers/WeChat/MenuController.php
@@ -21,31 +21,57 @@ class MenuController extends Controller
         $level1 = Menu::where(['pid' => 0])->orderBy('sort')->get();
         foreach($level1 as $k => $v) {
             if ($v->has_sub == 1) {
-                $buttons[$k]['name'] = $v->title;
+                $buttons[$k]['name'] = $v->name;
                 $buttons[$k]['sub_button'] = [];
                 $level2 = Menu::where(['pid' => $v->id])->orderBy('sort')->get();
                 foreach($level2 as $key => $val) {
-                    $buttons[$k]['sub_button'][$key]['type'] = 'view';
-                    $buttons[$k]['sub_button'][$key]['name'] = $val->title;
-                    $buttons[$k]['sub_button'][$key]['url'] = $val->url;
+                    if($val->type == 0) {
+                        $buttons[$k]['sub_button'][$key]['type'] = 'view';
+                        $buttons[$k]['sub_button'][$key]['name'] = $val->name;
+                        $buttons[$k]['sub_button'][$key]['url'] = $val->value;
+                    }elseif($val->type == 1) {
+                        $buttons[$k]['sub_button'][$key]['type'] = 'click';
+                        $buttons[$k]['sub_button'][$key]['name'] = $val->name;
+                        $buttons[$k]['sub_button'][$key]['key'] = $val->value;
+                    }elseif($val->type == 2) {
+                        $buttons[$k]['sub_button'][$key]['type'] = 'miniprogram';
+                        $buttons[$k]['sub_button'][$key]['name'] = $val->name;
+                        $buttons[$k]['sub_button'][$key]['appid'] = $val->app_id;
+                        $buttons[$k]['sub_button'][$key]['pagepath'] = $val->page_path;
+                    }
+
                 }
             } else {
-                $buttons[$k]['type'] = 'view';
-                $buttons[$k]['name'] = $v->title;
-                $buttons[$k]['url'] = $v->url;
+                if($val->type == 0) {
+                    $buttons[$k]['type'] = 'view';
+                    $buttons[$k]['name'] = $v->name;
+                    $buttons[$k]['url'] = $v->value;
+                }elseif($val->type == 1) {
+                    $buttons[$k]['type'] = 'click';
+                    $buttons[$k]['name'] = $v->name;
+                    $buttons[$k]['key'] = $v->value;
+                }elseif($val->type == 2) {
+                    $buttons[$k]['type'] = 'miniprogram';
+                    $buttons[$k]['name'] = $v->name;
+                    $buttons[$k]['appid'] = $v->app_id;
+                    $buttons[$k]['pagepath'] = $v->page_path;
+                }
+
             }
         }
 
         try{
-            $res = $this->menu->create($buttons);
-            if($res['errcode'] === 0 ) {
-                return webApiReturn(SUCCESS, '设置成功');
+            $res = $this->menu->add($buttons);
+            if ($res['errcode'] === 0) {
+                return webApiReturn(SUCCESS, '同步成功');
             } else {
-                return webApiReturn(SUCCESS, '设置失败');
+                var_dump($res);
+                return webApiReturn(ERROR, '同步失败');
             }
-        }catch (Exception $e) {
-            return webApiReturn(ERROR, '设置失败');
-        }
 
+        }catch (\Exception $e) {
+            var_dump($e);
+            return webApiReturn(ERROR, '同步失败');
+        }
     }
 }
